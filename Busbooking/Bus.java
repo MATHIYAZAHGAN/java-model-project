4be6c3001ff80a9b623b6aa8ff55c008ad1075f3
Mathiyazahgan <mathimathiyazahgan@gmail.com>
2024-07-18 23:29:00 +0530

    code change

--- Busbooking/Bus.java
+++ Busbooking/Bus.java
@@ -7,6 +7,7 @@ class Bus{
         Scanner scan = new Scanner (System.in);
         buses.add(new setc(1,100,true));
         buses.add(new setc(2,50,false));
+        while(true){
 
 
         for(setc bus: buses){
@@ -18,25 +19,39 @@ class Bus{
      
         System.out.println("Enter your choices !!");
         System.out.println("1------>Bookings");
-        System.out.println("2------>show booking details");
-        System.out.println("3------>cancel");
+        System.out.println("2------>cancel");
+        System.out.println("3------>show booking details");
 
         int choice = scan.nextInt();
          switch (choice) {
             case 1:
             Booking b =new Booking();
+            if(b.seatbooking(buses)){
+                System.out.println(" BOOKED ");
+            }else{
+                System.out.println("NOT ENOUGH SEATS");
+            }
             
                 break;
                 case 2:
-
+                scan.nextLine();
+                System.out.println("enter name to cancle...");
+                String name = scan.nextLine();
+                if (Booking.cancle(name,bookings,buses)){
+                    System.out.println("cancled...");
+                }
                 break;
                 case 3 :
+             
+                
                 break;
 
          
             default:
+            System.out.println("INVLID CREDENTIALS !!");
                 break;
          }
+        }
 
         
 
@@ -67,7 +82,7 @@ class setc{
         this.capcity=capcity;
 
     }
-    public int getcapacity(int capcity){
+    public int getcapacity(){
         return capcity;
     }
     public void setac(boolean ac){
@@ -98,7 +113,35 @@ class Booking{
 
 
     }
-    void seatbooking(){
+    static boolean cancle (String name , ArrayList<Booking> bookings, ArrayList<setc> buses){
+        for (Booking b : bookings){
+            if (b.name.equalsIgnoreCase(name)){
+                for (setc bus: buses){
+                    if (b.BusNo == bus.getbusNo()){
+                        bus.setcapacity(bus.getcapacity() + b.seat);
+                        bookings.remove(b);
+                        return true;
+                    }
+                }
+            }
+        }
 
+
+        return false;
     }
+
+    boolean seatbooking(ArrayList<setc>  buses){
+   
+        for(setc b :buses){
+            if(b.getbusNo()==BusNo){
+            if(b.getcapacity()>seat){
+
+                b.setcapacity(b.getcapacity()-seat);
+                return true;
+            }
+            }
+
+        }
+        return false;
+}
 }
